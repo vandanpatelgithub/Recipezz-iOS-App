be0bf525c61e091cf16f94fb3cc8f9f9693bef78
Vandan Patel <vandan.ets@gmail.com>
2016-08-08 22:33:20 -0700

    App works as intended!

---
+++ Recipezz/CreateRecipeVC.swift
@@ -0,0 +1,61 @@
+//
+//  CreateRecipeVC.swift
+//  Recipezz
+//
+//  Created by Preeti Patel on 08/08/16.
+//  Copyright © 2016 Vandan Patel. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CreateRecipeVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var recipeTitle: UITextField!
+    @IBOutlet weak var recipeIngredients: UITextField!
+    @IBOutlet weak var recipeSteps: UITextField!
+    @IBOutlet weak var recipeImg: UIImageView!
+    @IBOutlet weak var addRecipeButton: UIButton!
+    
+    var imagePicker: UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        recipeImg.layer.cornerRadius = 4.0
+        recipeImg.clipsToBounds = true
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        recipeImg.image = image
+    }
+    
+    @IBAction func addImage(sender: AnyObject) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func createRecipe(sender: UIButton!) {
+        if let title = recipeTitle.text where title != "" {
+            let app = UIApplication.sharedApplication().delegate as! AppDelegate
+            let context = app.managedObjectContext
+            let entity = NSEntityDescription.entityForName("Recipe", inManagedObjectContext: context)
+            let recipe = Recipe(entity: entity!, insertIntoManagedObjectContext: context)
+            
+            recipe.title = title
+            recipe.ingredients = recipeIngredients.text
+            recipe.steps = recipeSteps.text
+            recipe.setRecipeImage(recipeImg.image!)
+            
+            context.insertObject(recipe)
+            
+            do {
+                try context.save()
+            } catch {
+                print("Could not save Recipe!")
+            }
+            self.navigationController?.popViewControllerAnimated(true)
+        }
+    }
+}
--- Recipezz/ViewController.swift
+++ Recipezz/ViewController.swift
@@ -1,5 +1,6 @@
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -11,6 +12,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        fetchAndSetResults()
+        tableView.reloadData()
+    }
+    
+    func fetchAndSetResults() {
+        let app = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context = app.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "Recipe")
+        
+        do {
+            let results = try context.executeFetchRequest(fetchRequest)
+            self.recipes = results as! [Recipe]
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
